a88b1c9b9d42870385b3b4101ba7e21ed4c8fbf4
Andreas Kempe <andreas.kempe@byte-artist.de>
2021-11-20 17:57:30 +0100

    rename ConfigSchema to Configuration

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -5,7 +5,7 @@ namespace ByteArtist\MenuBundle\DependencyInjection;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 
-class ConfigSchema implements ConfigurationInterface
+class Configuration implements ConfigurationInterface
 {    
     public function getConfigTreeBuilder()    
     {      
