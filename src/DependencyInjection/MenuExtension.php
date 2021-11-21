23d032d7cd5f3d049adbb766777f154e7a113691
Andreas Kempe <andreas.kempe@byte-artist.de>
2021-11-22 00:49:08 +0100

    rename MenuBundleExtension to MenuExtension

--- src/DependencyInjection/MenuExtension.php
+++ src/DependencyInjection/MenuExtension.php
@@ -8,7 +8,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
-class MenuBundleExtension extends Extension
+class MenuExtension extends Extension
 {
     function load(array $configs, ContainerBuilder $containerBuilder)
     {
